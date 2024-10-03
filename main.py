ad4ddc8cc2edb83bf72f4f8caaf7a44e73d46f4c
MelnykIhor <melnyk70@gmail.com>
2024-10-03 22:17:56 +0300

    DZ_3_3

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# ДЗ 3.3. Розділити один список на два списки
+# 4 варіанти даних для вибору:
+#numbers=[]
+#numbers=[111]
+#numbers=[1,2,3,4,5,6]
+numbers=[1,2,3,4,5,6,7]
+# програма:
+len_numbers = len(numbers) #довжина списку
+len_numbers1=len_numbers%2 #значення для визначення парної або непарної кількості елементів
+len_numbers2=len_numbers//2 #значення для визначення парної або непарної кількості елементів
+if len_numbers == 0:
+   numbers1=numbers
+   numbers2=numbers
+   numbers3=[numbers1,numbers2]
+   print(numbers,"=>",numbers3)
+elif len_numbers1 ==0:
+   numbers1=numbers[0:len_numbers2:1]
+   numbers2=numbers[len_numbers2:numbers[-1]:1]
+   numbers3=[numbers1,numbers2]
+   print(numbers,"=>",numbers3)
+elif len_numbers1 ==1:
+   numbers1=numbers[0:len_numbers2+1:1]
+   numbers2=numbers[len_numbers2+1:numbers[-1]:1]
+   numbers3=[numbers1,numbers2]
+   print(numbers,"=>",numbers3)
+else:
+   print("Щось пішло не так :-)")
